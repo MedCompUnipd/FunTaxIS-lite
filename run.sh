73b66aecb7b80a8a090063e52786463965eb91c1
emilioispano <emilio.ispano@phd.unipd.it>
2023-02-07 16:02:59 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,21 @@ download=0
 full=0
 constraints=0
 
+usage() {
+    echo "Usage: $0 [-h <>] [-d <>] [-i <>] [-c <>] [-f <>] [-s <path/to/file>]" 1>&2
+    echo "-h prints this help message"
+    echo "-d flag to execute the download.sh script"
+    echo "-i flag to execute the generate_intermediate.sh script"
+    echo "-c flag to execute the generate_constraints.sh script"
+    echo "-f flag to execute the full pipeline, overrides any other flag"
+    echo "-s (REQUIRED) the configuration file"
+    exit 1
+}
+
+[ $# -eq 0 ] && usage
+
 # Options parser
-while getopts "idfcs:" opt
+while getopts "hidfcs:" opt
 do
     case "${opt}" in
         i) intermediate=1;;
@@ -14,6 +27,7 @@ do
         f) full=1;;
         c) constraints=1;;
         s) setting=${OPTARG};;
+        h | *) usage;;
     esac
 done
 
