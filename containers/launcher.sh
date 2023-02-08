ebdad89fadc7017d47bd31bbb348fbe5248bc259
emilioispano <62720938+emilioispano@users.noreply.github.com>
2023-02-08 11:11:15 +0100

    Update launcher.sh

--- containers/launcher.sh
+++ containers/launcher.sh
@@ -4,27 +4,28 @@ option='f'
 opt_tag=''
 
 # Options parser
-while getopts ':a:d:f:s:' opt
+while getopts ':i:d:f:c:' opt
 do
     case ${opt}
     in
-        a) option='a'; opt_tag=${OPTARG};;
+        i) option='i'; opt_tag=${OPTARG};;
         d) option='d'; opt_tag=${OPTARG};;
         f) option='f'; opt_tag=${OPTARG};;
-        s) option='s'; opt_tag=${OPTARG};;
+        c) option='c'; opt_tag=${OPTARG};;
     esac
 done
 
 # Based on the case run a specific script
 case ${option}
 in
-    a) bash ../generate_all_taxon_constraints.sh "${opt_tag}";;
+    i) bash ../generate_intermediate.sh "${opt_tag}";;
     d) bash ../download.sh "${opt_tag}";;
     f) bash ../run.sh "${opt_tag}";;
-    s) bash ../generate_species_taxon_constraints.sh "${opt_tag}";;
-    *) echo 'Wrong option. Available options are';\
-       echo '-a: to generate the taxonomic constraints of all the species found in the input data.';\
+    c) bash ../generate_constraints.sh "${opt_tag}";;
+    *) echo 'Wrong option. Available options are:';\
+       echo '-i: to generate the taxonomic constraints of all the species found in the input data.';\
        echo '-d: to download all the necessary data to correctly run FunTaxIS-lite.';\
        echo '-f: to use the complete FunTaxIS pipeline.';\
-       echo '-s: to generate the taxonomic constraints of the species chosen by the user.';;
+       echo '-c: to generate the taxonomic constraints of the species chosen by the user.';\
+       echo 'The configuration file must be passed as argument after the selected options.';;
 esac
