a4daf66ab0ddb33f6b6508229b5671e977eda7b7
emilioispano <emilio.ispano@phd.unipd.it>
2024-02-15 13:14:43 +0100

    code upgrade

--- download.sh
+++ download.sh
@@ -10,8 +10,9 @@ taxon_dl_site='https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz'
 # Parameters:
 #  $1: the file's download link.
 #  $2: the folder where we save the file.
-function download () {
-    wget "$1" -P "$2"
+#  $3: the filename.
+download() {
+    wget "$1" -O "$2"/"$3"
 }
 
 
@@ -19,13 +20,13 @@ function download () {
 # Parameters:
 #  $1: the compressed file.
 #  $2: the folder where we save the file.
-function decompress () {
+decompress() {
     if [[ "$1" =~ .*.tar.gz ]]
     then
        tar -xzf "$1" -C "$2"
     elif [[ "$1" =~ .*.gz ]]
     then
-      gzip -c -d "$1" > "$2""goa_uniprot_all.gaf"
+      gzip -c -d "$1" > "$2"/"goa_uniprot_all.gaf"
     fi
 
     rm "$1"
@@ -36,139 +37,203 @@ function decompress () {
 # Parameters:
 #  $1: the file's download link.
 #  $2: the folder where we save the file.
-function dl_and_decompress () {
-    download "$1" "$2"
+#  $3: the downloaded filename.
+dl_and_decompress() {
+    download "$1" "$2" "$3"
 
-    for file in "$2"*
-    do
-        decompress "${file}" "$2"
-    done
+    file="${2}/${3}"
+    decompress "${file}" "$2"
 }
 
 
 # Function to verify the presence of the gene ontology file. If not present, we download it.
 # Parameters:
 #  $1: the path where we save the gene ontology file.
-function verifyGoFilePresence() {
+verifyGoFilePresence() {
     # Count the number of file with .owl extention.
-    count=`ls -1 "$1"*.owl 2> /dev/null | wc -l`
-    if [ ! -d "$1" ]
-    then
+    count=$(ls -1 "$1"/go-plus.owl 2> /dev/null | wc -l)
+    if [ ! -d "$1" ]; then
         mkdir -p "$1"
-        download "${go_plus_dl_site}" "$1"
-    elif [ $count -eq 0 ]
-    then
-        download "${go_plus_dl_site}" "$1"
+        download "${go_plus_dl_site}" "$1" "go-plus.owl"
+    elif [ $count -eq 0 ]; then
+        download "${go_plus_dl_site}" "$1" "go-plus.owl"
+    else
+        echo "In the provided directory there exist already a go-plus.owl file, are you want to woverwrite it? (yes/no)"
+        read answer
+        case $answer in
+            "Y"|"y"|"YES"|"Yes"|"yes")
+                download "${go_plus_dl_site}" "$1" "go-plus.owl";;
+            *)
+                echo "Accepted affirmative answers are Y | YES | Yes | yes | y"
+                echo "Interrupting the script"
+                exit 1;;
+        esac
     fi
 }
 
 # Function to verify the presence of the gene ontology annotation file. If not present, we download it.
 # Parameters:
 #  $1: the path where we save the gene ontology annotation file.
-function verifyGoaFilePresence() {
+verifyGoaFilePresence() {
     # Count the number of file with .gaf extension.
-    count=`ls -1 "$1"*.gaf 2> /dev/null | wc -l`
-    if [ ! -d "$1" ]
-    then
+    if [ ! -d "$1" ]; then
         mkdir -p "$1"
-        dl_and_decompress "${goa_dl_site}" "$1"
-    elif [ $count -eq 0 ]
-    then
-        dl_and_decompress "${goa_dl_site}" "$1"
+        dl_and_decompress "${goa_dl_site}" "$1" "goa_uniprot_all.gaf.gz"
+    elif [ ! -f "$1"/goa_uniprot_all.gaf.gz ]; then
+        check=$(ls -1 "$1"/*.gaf 2> /dev/null | wc -l)
+        if [ $check -gt 0 ]; then
+            echo "In the provided directory there exist already a GAF file, do you want to download the latest goa_uniprot_all.gaf.gz? (yes/no)"
+            read answer
+            case $answer in
+                "Y"|"y"|"YES"|"Yes"|"yes")
+                    dl_and_decompress "${go_plus_dl_site}" "$1" "goa_uniprot_all.gaf.gz";;
+                *)
+                    echo "Accepted affirmative answers are Y | YES | Yes | yes | y"
+                    echo "Interrupting the script"
+                    exit 1;;
+            esac
+        else
+            dl_and_decompress "${goa_dl_site}" "$1" "goa_uniprot_all.gaf.gz"
+        fi
+    else
+        echo "In the provided directory there exist already a goa_uniprot_all.gaf.gz file, are you sure to overwrite it? (yes/no)"
+        read answer
+        case $answer in
+            "Y"|"y"|"YES"|"Yes"|"yes")
+                dl_and_decompress "${go_plus_dl_site}" "$1" "goa_uniprot_all.gaf.gz";;
+            *)
+                echo "Accepted affirmative answers are Y | YES | Yes | yes | y"
+                echo "Interrupting the script"
+                exit 1;;
+        esac
     fi
 }
 
 # Function to verify the presence of the taxonomy tree files. If not present, we download it.
 # Parameters:
 #  $1: the path where we save the taxonomy tree files.
-function verifyTaxonomyFilesPresence() {
+verifyTaxonomyFilesPresence() {
     # Count the number of file with .dmp extention.
-    count=`ls -1 "$1"*.dmp 2> /dev/null | wc -l`
-    if [ ! -d "$1" ]
-    then
+    if [ ! -d "$1" ]; then
         mkdir -p "$1"
-        dl_and_decompress "${taxon_dl_site}" "$1"
-    elif [ $count -eq 0 ]
-    then
-        dl_and_decompress "${taxon_dl_site}" "$1"
+        dl_and_decompress "${taxon_dl_site}" "$1" "taxdump.tar.gz"
+    elif [ ! -f "$1"/taxdump.tar.gz ]; then
+        check=$(ls -1 "$1"/*.dmp 2> /dev/null | wc -l)
+        if [ $check -gt 0 ]; then
+            echo "In the provided directory there exist already a taxdump folder, do you want to download the latest taxdump.tar.gz? (yes/no)"
+            read answer
+            case $answer in
+                "Y"|"y"|"YES"|"Yes"|"yes")
+                    dl_and_decompress "${taxon_dl_site}" "$1" "taxdump.tar.gz";;
+                *)
+                    echo "Accepted affirmative answers are Y | YES | Yes | yes | y"
+                    echo "Interrupting the script"
+                    exit 1;;
+            esac
+        else
+            dl_and_decompress "${taxon_dl_site}" "$1" "taxdump.tar.gz"
+        fi
+    else
+        echo "In the provided directory there exist already a taxdump.tar.gz file, are you sure to overwrite it? (yes/no)"
+        read answer
+        case $answer in
+            "Y"|"y"|"YES"|"Yes"|"yes")
+                dl_and_decompress "${taxon_dl_site}" "$1" "taxdump.tar.gz";;
+            *)
+                echo "Accepted affirmative answers are Y | YES | Yes | yes | y"
+                echo "Interrupting the script"
+                exit 1;;
+        esac
     fi
 }
 
 
-## Configuration file parser.
-declare -A config_array # Array declaration.
-
-if [[ ${#1} -eq 0 ]]
-then
-    echo 'Configuration file is required.'
-    exit 0
-fi
-
-IFS=":" # Internal Field Separator for the read command.
-# Read the configuration file.
-# Parameters:
-#  $1: the configuration file path.
-while read -r name v
-do
-    key=$(echo "${name}" | sed -e 's/^[ \t]*//')
-    value=$(echo "${v}" | sed -e 's/^[ \t]*//')
-
-    if [[ "$key" =~ ^#.*  ]] || [[ "$key" =~ ^--.* ]] || [[ ${#value} -eq 0 ]]
-    then
-       continue
-    fi
-
-    config_array+=( [$key]=$value )
-done < $1
-##
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "This script downloads from the latest version databases all the necessary data to run the FunTaxIS-lite pipeline, them being:"
+    echo " - go-plus.owl:           the GO graph file"
+    echo " - goa_uniprot_all.gaf:   the GOA gaf file"
+    echo " - taxondump/ :           the taxonomy taxondump folder"
+    echo "If no option is used, all files are downloaded in the default directory ./input/, specifically in sub-directories ./input/go/ , ./input/goa/ , ./input/taxonomy/"
+    echo ""
+    echo "OPTIONS:"
+    echo ""
+    echo "-w, --owl:        Folder where to save the go-plus.owl file (default is ./input/go/)."
+    echo "-g, --goa:        Folder where to save the goa_uniprot_all.gaf file (default is ./input/goa/)."
+    echo "-t, --taxonomy:   Folder where to save all the taxdump files (default is ./input/taxonomy/)."
+    exit 1
+}
 
 
-# Variables
-base_folder="${config_array[folder]}" # it's the path where all the files are saved. It's the path given by the parameter 'folder' in the configuration file.
-go_folder="${config_array[go]}" # It's the gene ontology file path. It's the path given by the parameter 'go' in the configuration file. If not provided, an empty string is saved.
-goa_folder="${config_array[goa]}" # It's the gene ontology annotation file path. It's the path given by the parameter 'goa' in the configuration file. If not provided, an empty string is saved.
-taxonomy_folder="${config_array[taxonomy]}" # It's the taxonomy folder path, It's the path given by the parameter 'taxonomy' in the configuration file. If not provided, an empty string is saved.
-output_folder="${config_array[results]}/" # It's the results folder path. It's given by the parameter 'results' in the configuration file.
-type="${config_array[type]}" # It's the type of taxonomic constraints we want to be generated. It's given by the parameter 'type' in the configuration file. If not provided an empty string is saved and all the type (manual, automatic) are used.
-debug="${config_array[debug]}" # It's  the intermediate files are removed or not. It's given by the parameter 'debug' in the configuration file. If not provided an empty string is saved and the intermediate files are removed.
-used_go='' # It's the gene ontology file name.
-used_goa='goa_uniprot_all.gaf' # It's the gene ontology annotation file name.
+# DEFAULT VALUES
+go_folder="./input/go/"                 # It's the gene ontology file path.
+goa_folder="./input/goa/"               # It's the gene ontology annotation file path.
+taxonomy_folder="./input/taxonomy/"     # It's the taxonomy folder path.
 
+download_all=1
+download_owl=0
+download_goa=0
+download_taxonomy=0
 
-# Sets the taxonomy folder if not defined in the configuration file.
-if [[ ${#taxonomy_folder} -eq 0 ]]
-then
-   taxonomy_folder="${base_folder}input/taxonomy/"
-fi
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -w|--owl)
+            if [[ $# -gt 1 && ! $2 == -* ]]; then
+                go_folder="$2"
+                download_owl=1
+                download_all=0
+            else
+                echo "Expected argument after option -w/--owl!"
+                usage
+            fi
+            shift
+            shift;;
 
-# Sets the gene ontology folder and the gene ontology file name if the gene ontology file path is not defined in the configuration file.
-# Otherwise, from the given path extracts the folder path and the file name.
-if [[ ${#go_folder} -eq 0 ]]
-then
-    go_folder="${base_folder}input/go/"
-    used_go='go-plus.owl'
-else
-    case "${go_folder}" in
-        *go-plus.owl*) used_go=$(echo "${go_folder}" | awk -F/ '{print $NF}'); go_folder=${go_folder%"${used_go}"} ;; # Extract the gene ontology file name and the path.
-        *) echo 'ERROR: Gene ontology graph is not in the owl format or is not the plus version.'; exit 1 ;;
+        -g|--goa)
+            if [[ $# -gt 1 && ! $2 == -* ]]; then
+                goa_folder="$2"
+                download_goa=1
+                download_all=0
+            else
+                echo "Expected argument after option -g/--goa!"
+                usage
+            fi
+            shift
+            shift;;
+
+        -t|--taxonomy)
+            if [[ $# -gt 1 && ! $2 == -* ]]; then
+                taxonomy_folder="$2"
+                download_taxonomy=1
+                download_all=0
+            else
+                echo "Expected argument after option -t/--taxonomy!"
+                usage
+            fi
+            shift
+            shift;;
+        -h|--help)
+            usage;;
+        *)
+            # unknown option
+            echo "Error: Unknown option $1"
+            usage
+            ;;
     esac
+done
+
+# Check
+if [ $download_all -eq 1 ] || [ $download_owl -eq 1 ]; then
+    verifyGoFilePresence "${go_folder}"
 fi
 
-# Sets the gene ontology annotation folder if the goa file path is not defined in the configuration file.
-# Otherwise, from the given path extracts the folder path and the file name.
-if [[ ${#goa_folder} -eq 0 ]]
-then
-    goa_folder="${base_folder}input/goa/"
-else
-    used_goa=$(echo "${goa_folder}" | awk -F/ '{print $NF}')
-    goa_folder=${goa_folder%"${used_goa}"}
+# Check
+if [ $download_all -eq 1 ] || [ $download_taxonomy -eq 1 ]; then
+    verifyTaxonomyFilesPresence "${taxonomy_folder}"
 fi
 
-# Check if the gene ontology and the taxonomy files are present.
-verifyGoFilePresence "${go_folder}"
-verifyTaxonomyFilesPresence "${taxonomy_folder}"
-# Check if the gene ontology annotation is present only if the type of constraints isn't "goc".
-case "${type}" in
-    "GOConsortium"|"goc"|"g" ) ;;
-    * ) verifyGoaFilePresence "${goa_folder}"
-esac
+# Check
+if [ $download_all -eq 1 ] || [ $download_goa -eq 1 ]; then
+    verifyGoaFilePresence "${goa_folder}"
+fi
